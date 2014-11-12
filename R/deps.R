3307cc5d7da96229d893ed697a13d9c0d3f81288
Winston Chang <winston@stdout.org>
2014-11-12 14:43:11 -0600

    Update to Font Awesome 4.2.0

--- R/deps.R
+++ R/deps.R
@@ -1,6 +1,6 @@
 dashboardDeps <- list(
-  htmlDependency("font-awesome", "4.1.0",
-    src = c(href = "http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/"),
+  htmlDependency("font-awesome", "4.2.0",
+    src = c(href = "http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/"),
     stylesheet = "font-awesome.min.css"
   ),
   htmlDependency("ionicons", "1.5.2",
