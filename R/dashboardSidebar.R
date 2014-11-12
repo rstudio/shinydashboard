2180921dfd36675b4365d6e545a9c2fa1a4f259b
Winston Chang <winston@stdout.org>
2014-11-11 21:43:57 -0600

    Add sidebar functions

---
+++ R/dashboardSidebar.R
@@ -0,0 +1,59 @@
+#' @export
+dashboardSidebar <- function(...) {
+  tags$section(class = "sidebar",
+    list(...)
+  )
+}
+
+#' @export
+sidebarUserPanel <- function(name) {
+  div(class = "user-panel",
+    div(class = "pull-left image",
+      img(src = "foo.png",  class = "img-circle", alt = "User Image")
+    ),
+    div(class = "pull-left info",
+      p(paste("Hello,", name)),
+      a(href = "#", tags$i(class = "fa fa-circle text-success"), "Online")
+    )
+  )
+}
+
+#' @export
+sidebarSearchForm <- function(textId, buttonId, label = "Search...") {
+  tags$form(class = "sidebar-form",
+    div(class = "input-group",
+      tags$input(id = textId, type = "text", class = "form-control",
+        placeholder = label
+      ),
+      span(class = "input-group-btn",
+        tags$button(id = buttonId, type = "button",
+          class = "btn btn-flat action-button",
+          tags$i(class = "fa fa-search")
+        )
+      )
+    )
+  )
+}
+
+#' @export
+sidebarMenu <- function(...) {
+  items <- list(...)
+
+  # Make sure the items are li tags
+  lapply(items, tagAssert, type = "li")
+
+  tags$ul(class = "sidebar-menu",
+    ...
+  )
+}
+
+#' @export
+menuItem <- function(text, icon, ...) {
+  tags$li(
+    a(href = "#",
+      tags$i(class = getIconClass(icon)),
+      span(text),
+      ...
+    )
+  )
+}
