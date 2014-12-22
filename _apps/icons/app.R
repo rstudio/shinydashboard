3ac208309c9a6eb29284da51ba7da16874784b5f
Winston Chang <winston@stdout.org>
2014-12-22 15:08:33 -0600

    Add icon section

---
+++ _apps/icons/app.R
@@ -0,0 +1,14 @@
+library(shinydashboard)
+
+ui <- bootstrapPage(
+  span(id = "content",
+    "Calendar from Font-Awesome:", icon("calendar"),
+    br(),
+    br(),
+    "Cog from Glyphicons:", icon("cog", lib = "glyphicon")
+  )
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
