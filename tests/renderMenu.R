6d02858c18a28297363d594724386e247d8c53e1
dmpe <cincenko@seznam.cz>
2015-10-31 22:18:09 +0100

    add standalone tests so that one can use them in the tests folder
    add really big dashboard covering about 70-80% of all elements used or described.
    update news file as I believe version update to AdminLTE 2.3.2 is fine and not going to break anything.

---
+++ tests/renderMenu.R
@@ -0,0 +1,19 @@
+library(shiny)
+# ========== Dynamic sidebarMenu ==========
+ui <- dashboardPage(
+  dashboardHeader(title = "Dynamic sidebar"),
+  dashboardSidebar(
+    sidebarMenuOutput("menu")
+  ),
+  dashboardBody()
+)
+
+server <- function(input, output) {
+  output$menu <- renderMenu({
+    sidebarMenu(
+      menuItem("Menu item", icon = icon("calendar"))
+    )
+  })
+}
+
+shinyApp(ui, server)
