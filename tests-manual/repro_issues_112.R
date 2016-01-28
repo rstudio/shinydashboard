4eff21fd4e634b740427cc4a19716b454d73da57
dmpe <cincenko@seznam.cz>
2016-01-28 20:38:38 +0100

    bump minor shiny version delete Rcpp from travis add 2 new repro cases for new issues

---
+++ tests-manual/repro_issues_112.R
@@ -0,0 +1,23 @@
+ui <- dashboardPage(
+  dashboardHeader(
+    title = "Sidebar spill"
+
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(text = "sfsdf sfaosh oas fwue wi aseiu wehw wuer woeur owuer  ")
+    )
+  ),
+  dashboardBody(
+    fluidRow(
+
+    )
+  )
+)
+
+server <- function(input, output) {
+
+}
+
+shinyApp(ui, server)
+
---
+++ tests-manual/repro_issues_113.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(shinydashboard)
+
+ui = shinyUI(dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    box(title = "Report", width = 12,
+        verbatimTextOutput("protocol")
+    )
+
+  )
+))
+
+server = shinyServer(function(input, output, session) {
+  output$protocol <- renderPrint({
+    print(numeric(10e3))
+  })
+})
+
+shinyApp(ui, server)
