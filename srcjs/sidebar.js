6e5c18cdbb83c8f7c101364d38867dbf18f50a2b
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-06-09 12:04:36 -0700

    more edge case fixing

--- srcjs/sidebar.js
+++ srcjs/sidebar.js
@@ -5,6 +5,17 @@ if ($("section.sidebar").data("disable")) {
   $(".navbar > .sidebar-toggle").hide();
 }
 
+// Get the correct value for `input$sidebarCollapsed`, depending on
+// whether or not the left offset on the sidebar is negative (hidden
+// - so `input$sidebarCollapsed` should be TRUE) or 0 (shown - so
+// `input$sidebarCollapsed` should be FALSE). That we know of,
+// `$(".main-sidebar").is(":visible")` is always true, so there is
+// no need to check for that.
+var sidebarCollapsedValue = function() {
+  if ($(".main-sidebar").offset().left < 0) return(true);
+  else return(false);
+};
+
 // Whenever the sidebar changes from collapsed to expanded and vice versa,
 // call this function, so that we can trigger the resize event on the rest
 // of the window and also update the value for the sidebar's input binding.
@@ -15,7 +26,7 @@ var sidebarChange = function() {
   // 2) Update the value for the sidebar's input binding
   var $obj = $('.main-sidebar.shiny-bound-input');
   var inputBinding = $obj.data('shiny-input-binding');
-  inputBinding.toggleValue($obj);
+  inputBinding.setValue($obj, sidebarCollapsedValue());
   $obj.trigger('change');
 };
 
@@ -24,6 +35,16 @@ var sidebarChange = function() {
 $( ".main-sidebar" ).on(
   'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
     sidebarChange);
+
+// This fixes an edge case: when the app starts up with the sidebar already
+// collapsed (either because the screen is small or because
+// `dashboardSidebar(disable = TRUE`), make sure that `input$sidebarCollapsed`
+// is set to `FALSE`. Whenever this is the case, `$(".main-sidebar").offset().left`
+// is negative. That we know of, `$(".main-sidebar").is(":visible")` is always
+// true, so there is no need to check for that.
+if ($(".main-sidebar").offset().left < 0) {
+  $(".main-sidebar").attr("data-collapsed", "true");
+}
 
 // Whenever we expand a menuItem (to be expandable, it must have children),
 // update the value for the expandedItem's input binding (this is the
