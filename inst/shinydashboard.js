be031e9af353c7ae4d9e0599ebf74907d2acbb76
Cole Arendt <cole.arendt@outlook.com>
2018-12-15 23:50:00 -0500

    update inst js files

--- inst/shinydashboard.js
+++ inst/shinydashboard.js
@@ -317,6 +317,27 @@ Shiny.inputBindings.register(sidebarmenuExpandedInputBinding,
   'shinydashboard.sidebarmenuExpandedInputBinding');
 
 //---------------------------------------------------------------------
+// Source file: ../srcjs/message_handler.js
+
+
+
+Shiny.addCustomMessageHandler('streamBox', function(data) {
+    $.each(data, function(key, val){
+      // get element
+      el = document.getElementById(key);
+
+      // update value
+      if (el.className.includes("small-box")) {
+        // for valueBox
+        el.getElementsByClassName("value-box-value")[0].innerText = val;
+      } else if (el.className.includes("info-box")) {
+        // for infoBox
+        el.getElementsByClassName("info-box-number")[0].innerText = val;
+      }
+    });
+  });
+
+//---------------------------------------------------------------------
 // Source file: ../srcjs/_end.js
 
 });
