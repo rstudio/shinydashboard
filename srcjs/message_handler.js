e4e8b8548c14726b6580aeee1dcbc353206c290d
Cole Arendt <cole.arendt@outlook.com>
2018-12-15 23:38:10 -0500

    simplify lookup process

--- srcjs/message_handler.js
+++ srcjs/message_handler.js
@@ -4,6 +4,6 @@ Shiny.addCustomMessageHandler('streamValueBox', function(data) {
     $.each(data, function(key, val){
       // update the value (in unhealthy fashion)
       el = document.getElementById(key);
-      el.getElementsByClassName("inner")[0].getElementsByTagName("h3")[0].innerText = val;
+      el.getElementsByClassName("valuebox-value")[0].innerText = val;
     });
   });
