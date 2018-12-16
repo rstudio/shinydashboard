7ed77190cccfbb0621ca4affeae9c7e4a457a156
Cole Arendt <cole.arendt@outlook.com>
2018-12-15 23:43:27 -0500

    update message handler to handle both value and info boxes

--- srcjs/message_handler.js
+++ srcjs/message_handler.js
@@ -4,6 +4,11 @@ Shiny.addCustomMessageHandler('streamValueBox', function(data) {
     $.each(data, function(key, val){
       // update the value (in unhealthy fashion)
       el = document.getElementById(key);
-      el.getElementsByClassName("valuebox-value")[0].innerText = val;
+
+      if (el.className.includes("small-box")) {
+        el.getElementsByClassName("valuebox-value")[0].innerText = val;
+      } else if (el.className.includes("info-box")) {
+        el.getElementsByClassName("info-box-number")[0].innerText = val;
+      }
     });
   });
