3a6d5388087902dadd7ae3c22d0ab3f7bded622f
Cole Arendt <cole.arendt@outlook.com>
2018-12-17 21:40:33 -0500

    update rendered js

--- inst/shinydashboard.js
+++ inst/shinydashboard.js
@@ -326,11 +326,13 @@ Shiny.addCustomMessageHandler('streamBox', function(data) {
       // get element
       el = document.getElementById(key);
 
+      // TODO: Error handling... what if el does not exist?
+
       // update value
-      if (el.className.includes("small-box")) {
+      if (el.classList.contains("small-box")) {
         // for valueBox
         el.getElementsByClassName("value-box-value")[0].innerText = val;
-      } else if (el.className.includes("info-box")) {
+      } else if (el.classList.contains("info-box")) {
         // for infoBox
         el.getElementsByClassName("info-box-number")[0].innerText = val;
       }
