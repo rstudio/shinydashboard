de857104eca5bb6271370e792451ad4ef05f915c
Carson <cpsievert1@gmail.com>
2021-02-23 08:42:57 -0600

    Grab AdminLTE 2.3.8 less src and convert to scss

--- tools/updateAdminLTE.R
+++ tools/updateAdminLTE.R
@@ -1,28 +1,51 @@
-#!/usr/bin/env Rscript
+library(sass)
+library(brio)
 
-# This script copies resources from AdminLTE to shinydashboard's inst
-# directory. The AdminLTE/ project directory should be on the same level
-# as the shinydashboard/ project directory.
+target <- rprojroot::find_package_root_file("inst/AdminLTE")
 
-
-# Returns the file currently being sourced or run with Rscript
-thisFile <- function() {
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  needle <- "--file="
-  match <- grep(needle, cmdArgs)
-  if (length(match) > 0) {
-    # Rscript
-    return(normalizePath(sub(needle, "", cmdArgs[match])))
-  } else {
-    # 'source'd via R console
-    return(normalizePath(sys.frames()[[1]]$ofile))
-  }
+download_gh_release <- function(repo, version) {
+  src_zip <- paste0("https://github.com/", repo, "/archive/v", version, ".zip")
+  tmp_zip <- tempfile(fileext = ".zip")
+  download.file(src_zip, tmp_zip)
+  tmp_dir <- tempfile()
+  dir.create(tmp_dir)
+  unzip(tmp_zip, exdir = tmp_dir)
+  file.path(tmp_dir, paste0(basename(repo), "-", version))
 }
 
-srcdir <- file.path(dirname(thisFile()), "../../AdminLTE/dist")
-destdir <- file.path(dirname(thisFile()), "../inst/AdminLTE")
+# This port of AdminLTE's less to sass implicitly assumes AdminLTE 2.4.10
+src <- download_gh_release("onkbear/admin-lte-2-sass", "0.0.2")
 
+# Remove old scss source files
+target_scss <- file.path(target, "scss")
+unlink(target_scss, recursive = TRUE)
+dir.create(target_scss)
 
-file.copy(file.path(srcdir, "/js/app.js"), "../srcjs/AdminLTE", overwrite = TRUE)
-file.copy(file.path(srcdir, "/css/AdminLTE.css"), destdir, overwrite = TRUE)
-file.copy(file.path(srcdir, "/css/skins/_all-skins.css"), destdir, overwrite = TRUE)
+# Copy over to package, then patch the entrypoint to make it so scss can be
+# compiled "standalone" (i.e., just with BS3 headers as we do below)
+file.copy(
+  dir(file.path(src, "assets/scss"), recursive = TRUE, full.names = TRUE),
+  target_scss
+)
+f <- file.path(target_scss, "AdminLTE.scss")
+adminlte <- read_lines(f)
+adminlte[[grep("bootstrap-sass", adminlte)]] <- ".clearfix { @include clearfix; }"
+write_lines(adminlte, f)
+
+# Pre-compile with BS3 headers
+withr::with_dir(target_scss, {
+  sass_partial(
+    sass_file("AdminLTE.scss"),
+    bundle = bslib::bs_theme(version = 3),
+    output = "../AdminLTE.css",
+    cache = NULL, write_attachments = FALSE
+  )
+})
+
+# Now grab the corresponding JavaScript
+src <- download_gh_release("ColorlibHQ/AdminLTE", "2.4.10")
+file.copy(
+  file.path(src, "dist/js/adminlte.js"),
+  rprojroot::find_package_root_file("srcjs/AdminLTE/app.js"),
+  overwrite = TRUE
+)
